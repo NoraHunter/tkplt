4c95e5c1b4a6cb76864d13d12c66bba6c11b4598
NoraHunter <122792624+NoraHunter@users.noreply.github.com>
2024-05-06 21:30:24 +0500

    Update plt_main.py

--- plt_main.py
+++ plt_main.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.geometry("1020x120")
+root.geometry("1280x120")
 root.title('')
-#root.attributes("-transparentcolor", "white")
-root.config(bg="white")
-#root.overrideredirect(True)
-#root.wm_attributes("-topmost", True)
+root.attributes("-transparentcolor", "green")
+root.config(bg="green")
+root.overrideredirect(True)
+root.wm_attributes("-topmost", True)
 '''def print_size(e):
     print(root.winfo_screenwidth(), root.winfo_screenheight())
 root.bind("<Configure>", print_size)'''
@@ -15,7 +15,7 @@ root.bind("<Configure>", print_size)'''
 def move_window(e):
     global root
     root.geometry(f"+{e.x_root}+{e.y_root}")
-win_move = tk.Label(root, text="||||||||||||||||||||||||||||||||||||||")
+win_move = tk.Label(root, text="                                                    ", bg="white")
 win_move.pack()
 win_move.bind("<B1-Motion>", move_window)
 
@@ -73,14 +73,11 @@ buttons = []
 # horizontalCharsFrame
 horizontalCharsFrame = tk.Frame(root)
 
-for i in range(len(button_card_list)):
-    buttons.append(tk.Button(horizontalCharsFrame, text=button_card_list[i][BUTTON_NAME], bd=-2))
-
 def change_char(e, card):
     global card_el_index, current_cards, card_lbl, found_lbl
     card_el_index = 0
     current_cards = card
-    found_lbl.config(text=f"  found : {len(current_cards)}  ")
+    found_lbl.config(text=f"  F : {len(current_cards)}  ")
     if len(current_cards) > 0:
         card_lbl.config(text=current_cards[card_el_index])
     #print(f"current_cards={current_cards}")
@@ -95,25 +92,40 @@ def search_in_cards(e):
                 result.append(card)
     change_char(e, card=result)
 
+def clear_input(e):
+    inputtxt.delete("1.0", "end-1c")
+
+def exit_event(e):
+    exit()
+
+for i in range(len(button_card_list)):
+    buttons.append(tk.Button(horizontalCharsFrame, text=button_card_list[i][BUTTON_NAME], bd=-2, bg="white", fg="#969696"))
+
 for i in range(len(buttons)):
     buttons[i].grid(row = 0, column = i)
     buttons[i].bind('<Button-1>', lambda e, card=button_card_list[i][BUTTON_CARDS]: change_char(e, card))
 else:
     charsLastBtnInd = len(buttons)
-    found_lbl = tk.Label(horizontalCharsFrame, text=f"  found : {len(current_cards)}  ")
+    found_lbl = tk.Label(horizontalCharsFrame, text=f"  F : {len(current_cards)}  ", bg="white", fg="#969696")
     found_lbl.grid(row=0, column=charsLastBtnInd)
-    inputtxt = tk.Text(horizontalCharsFrame, bg = "gray", width=10, height=1)
+
+    inputtxt = tk.Text(horizontalCharsFrame, width=10, height=1, bd=0, bg="white", fg="#969696")
     inputtxt.grid(row=0, column=charsLastBtnInd + 1)
-    search_btn = tk.Button(horizontalCharsFrame, text="Sch")
-    search_btn.grid(row=0, column=charsLastBtnInd + 2)
-    search_btn.bind('<Button-1>', search_in_cards)
-    #lambda e, card=search_in_cards(inputtxt.get(1.0, tk.END)): change_char(e, card)
+    inputtxt.bind("<Key>", search_in_cards)
+
+    clear_input_btn = tk.Button(horizontalCharsFrame, text="C", bd=-2, padx=10, bg="white", fg="#969696")
+    clear_input_btn.grid(row=0, column=charsLastBtnInd + 2)
+    clear_input_btn.bind("<Button-1>", clear_input)
+
+    exit_btn = tk.Button(horizontalCharsFrame, text="E", bd=-2, bg="white", fg="#969696")
+    exit_btn.grid(row=0, column=charsLastBtnInd + 3)
+    exit_btn.bind("<Double-Button-1>", exit_event)
     
 horizontalCharsFrame.pack()
 # horizontalCharsFrame
 
 # cards start
-card_lbl = tk.Label(root, text=current_cards[0], width=100)
+card_lbl = tk.Label(root, text=current_cards[0], bg="white", fg="#969696")
 card_lbl.pack()
 # cards end
 
@@ -121,7 +133,7 @@ card_lbl.pack()
 # horizontalCardsFrame start
 horizontalCardsFrame = tk.Frame(root)
 
-prev_ans_bt = tk.Button(horizontalCardsFrame, text="<")
+prev_ans_bt = tk.Button(horizontalCardsFrame, text="<", bd=-2, bg="white", fg="#969696")
 prev_ans_bt.grid(row = 0, column = 0)
 def prev_ans_ev(e):
     global card_el_index
@@ -131,7 +143,7 @@ def prev_ans_ev(e):
     card_lbl.config(text=current_cards[card_el_index])
 prev_ans_bt.bind("<Button-1>", prev_ans_ev)
 
-next_ans_bt = tk.Button(horizontalCardsFrame, text=">")
+next_ans_bt = tk.Button(horizontalCardsFrame, text=">", bd=-2, bg="white", fg="#969696")
 next_ans_bt.grid(row = 0, column = 1)
 def next_ans_ev(e):
     global card_el_index
